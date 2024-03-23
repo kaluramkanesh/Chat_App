9ddc0b7637e0076f1340fabbc0c44301092e9547
kaluramkanesh <kaluramkanesh123@gmail.com>
2024-03-23 17:10:09 +0530

    model created

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
 const router = require("./routes/routers")
+const path = require("path")
+const multer=require("multer")
 const app = express()
 const PORT = process.env.PORT || 3123
 const MONGODBURL = process.env.MONGODB_URL
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Set up views directory and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('./public'))
 mongoose.connect(MONGODBURL, {
 
 }).then(() => {
